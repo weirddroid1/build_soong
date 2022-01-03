3ce31fdb6dfce282a9b311cd5705c271251cdae9
Ali B <abittin@gmail.com>
2022-01-03 21:53:08 +0300

    soong: Whitelist mogrify and shuf for aicp's bootanimation
    Required for randomising and optimising the bootanimation
    
    Change-Id: I77fb45e5d9e2333cbdd733eafa7b09d0f8948751

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -110,6 +110,10 @@ var Configuration = map[string]PathConfig{
 	"unzip":       Allowed,
 	"zip":         Allowed,
 
+	// AICP specific tools
+	"shuf":           Allowed,
+	"mogrify":        Allowed,
+
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
