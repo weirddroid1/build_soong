a6951f6a429159391c7348d93ff51bd767a96a4e
SpiritCroc <dev@spiritcroc.de>
2025-07-06 20:05:41 +0200

    soong: Tweak props for userdebug (set adb secure; allow mock locations)
    Change-Id: I23e123a0cfc28be73ebbd06a2c356c276ac787ac

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -382,6 +382,10 @@ def append_additional_system_props(args):
       enable_target_debugging = False
       enable_dalvik_lock_contention_logging = False
     else:
+      # AICP: also set ro.adb.secure for userdebug
+      if config["BuildVariant"] == "userdebug":
+        props.append("ro.adb.secure=1")
+
       # Disable debugging in userdebug builds if PRODUCT_NOT_DEBUGGABLE_IN_USERDEBUG
       # is set.
       if config["ProductNotDebuggableInUserdebug"]:
@@ -389,7 +393,11 @@ def append_additional_system_props(args):
         enable_dalvik_lock_contention_logging = False
 
     # Disallow mock locations by default for user builds
-    props.append("ro.allow.mock.location=0")
+    # AICP: still allow for userdebug
+    if config["BuildVariant"] == "userdebug":
+      props.append("ro.allow.mock.location=1")
+    else:
+      props.append("ro.allow.mock.location=0")
   else:
     # Turn on checkjni for non-user builds.
     props.append("ro.kernel.android.checkjni=1")
